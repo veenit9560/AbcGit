256eef5725275bdfd523d5546cfee8615d0411c8
veenit kumar <veenit704kumar@gmail.com>
2023-03-29 16:13:52 +0530

    python file

---
+++ main.py
@@ -0,0 +1,18 @@
+# print("hello veenit ")
+numbers=[42,56,78,63,40,63]
+print(numbers[3])
+numbers[3]=88
+print(numbers[3])
+print(numbers)
+print(numbers[1:4])
+# numbers.append(50)
+numbers.insert(3,'hello')
+print(numbers)
+numbers.remove('hello')
+print(numbers)
+
+print()
+
+print(max(numbers))
+print(min(numbers))
+print(len(numbers))
