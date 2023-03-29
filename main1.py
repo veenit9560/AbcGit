06e04fe87b84ba38c67f65603957faf0eae42962
veenit kumar <veenit704kumar@gmail.com>
2023-03-29 16:19:21 +0530

    don

---
+++ main1.py
@@ -0,0 +1,12 @@
+# dictionaries
+names={'veenit':23,
+       'sunita':19,
+       'avneet': 15,
+       'kaveeta':10}
+
+print(names['avneet'])
+print(names['kaveeta'])
+names['avneet']=12
+print(names['avneet'])
+print(names.keys())
+print(names.values())
